7a9930dbd12f980506ec44ff2752c3e22534015c
RickyMandich <ricky.mandich@gmail.com>
2024-09-21 11:56:18 +0200

    aggiornamento 2024 09 21 11:56

---
+++ src/main/java/com/example/sitoParrocchia/SitoParrocchiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sitoParrocchia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitoParrocchiaApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SitoParrocchiaApplication.class, args);
+    }
+
+}
