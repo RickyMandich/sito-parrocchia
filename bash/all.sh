023a6ad03640dfa206bee99ff879366682700432
RickyMandich <ricky.mandich@gmail.com>
2024-11-11 16:22:48 +0100

    aggiornamento 2024 11 11 16:22

--- bash/all.sh
+++ bash/all.sh
@@ -8,7 +8,7 @@ function uploadFilesFromCommit() {
         # Costruisci il percorso FTP per il file
         local relativePath=$(dirname "$file")
         local fileName=$(basename "$file")
-        local ftpRequest="ftp://swudb:Minecraft35%3F@ftp.swudb.altervista.org:21/$relativePath/$fileName"
+        local ftpRequest="ftp://santifrancescoechiara:Gubbio22@ftp.santifrancescoechiara.altervista.org:21/$relativePath/$fileName"
 
         # Esegui il comando curl per caricare il file
         local curlCommand="curl -T \"$file\" \"$ftpRequest\" --ftp-pasv --ftp-create-dirs"
