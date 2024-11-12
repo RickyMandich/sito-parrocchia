6e9047d7e8e8dad2b3995745907862ba0e06921e
RickyMandich <ricky.mandich@gmail.com>
2024-11-12 13:40:09 +0100

    aggiornamento 2024 11 12 13:40

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it" xmlns="">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Francesco & Chiara</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="js.js"></script>
+    </head>
+    <body>
+        <?php require_once "instestazione.php";?>
+        <?php require_once "navigatore.php";?>
+        <main>
+            <br><br><br><br><br><br><br><br>
+        </main>
+        <?php require_once "footer.php";?>
+        <span id="test"></span>
+    </body>
+</html>
