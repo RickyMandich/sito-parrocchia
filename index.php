8717afbffb6f52c1d3ccedb8a122f81ed080a1dc
RickyMandich <ricky.mandich@gmail.com>
2024-11-12 14:00:43 +0100

    aggiornamento 2024 11 12 14:00

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require_once "header.php";?>
 <!DOCTYPE html>
-<html lang="it" xmlns="">
+<html lang="it" xmlns="" class="<?php echo $file?>">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,6 +16,7 @@
             <br>
             <br>
             <br>
+            <?php echo $file?>
             <br>
             <br>
             <br>
