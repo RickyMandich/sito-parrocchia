917bf05046030d3fc25e301cea4de9da63d06121
RickyMandich <ricky.mandich@gmail.com>
2024-11-13 08:57:44 +0100

    aggiornamento 2024 11 13 08:57

--- articolo.php
+++ articolo.php
@@ -4,14 +4,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php ?></title>
+        <title><?php echo $conn->query("select * from articoli where id = ".$_GET["id"])->fetch_assoc()["titolo"];?></title>
     </head>
     <body>
-        articolo
-        <?php
-        if(isset($_GET["id"])):
-            echo $_GET["id"];
-        endif;
-        var_dump($_GET);?>
+        
     </body>
 </html>
--- header.php
+++ header.php
@@ -3,3 +3,7 @@ require_once "classi/Articolo.php";
 $file = basename($_SERVER['PHP_SELF']);
 $file = preg_replace('/\?.*/', '', $file);
 $file = preg_replace('/\.php$/', '', $file);
+$conn = new mysqli(hostname: "localhost",username: "santifrancescoechiara", database:"my_santifrancescoechiara", port:3306);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
