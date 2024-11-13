1ee26b687e438a21656d28932a25d7241857457a
RickyMandich <ricky.mandich@gmail.com>
2024-11-13 18:24:06 +0100

    aggiornamento 2024 11 13 18:24

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <?php
 require_once "classi/Articolo.php";
+require_once "classi/Utente.php";
 $file = basename($_SERVER['PHP_SELF']);
 $file = preg_replace('/\?.*/', '', $file);
 $file = preg_replace('/\.php$/', '', $file);
