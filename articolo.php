fb8e189a6b0b71c025600ca4fbd80b4c348c308d
RickyMandich <ricky.mandich@gmail.com>
2024-11-13 08:38:34 +0100

    aggiornamento 2024 11 13 08:38

---
+++ articolo.php
@@ -0,0 +1,14 @@
+<?php require_once "header.php"?>
+<!DOCTYPE html>
+<html lang="en" class="<?php echo $file?>">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php ?></title>
+    </head>
+    <body>
+        articolo
+        <?php echo $_GET["id"];
+        var_dump($_GET);?>
+    </body>
+</html>
