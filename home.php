df6d1ef099820a7ead82abd296bf119e5830825c
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 17:15:01 +0100

    aggiornamento 2024 11 14 17:15

--- home.php
+++ home.php
@@ -23,7 +23,7 @@
                 </span>
                 <?php
             };?>
-            <?php $resultSet = $conn->query("select * from articoli where pinnato = false order by id desc limit 0");
+            <?php $resultSet = $conn->query("select * from articoli where pinnato = false order by id desc limit 3");
             while($articolo=$resultSet->fetch_assoc()){
                 ?>
                 <span class="anteprimaArticolo">
