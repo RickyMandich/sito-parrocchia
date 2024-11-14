f1a631c7a2c500add5214fdd6b11eec32b2dfb63
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 20:17:11 +0100

    aggiornamento 2024 11 14 20:17

--- creaArticolo.php
+++ creaArticolo.php
@@ -14,7 +14,8 @@ if(!isset($_SESSION["user"])){
     <body>
         <?php if(isset($_POST["titolo"])){
             if($conn->query("select * from articoli where titolo = '".$_POST["titolo"]."'")->fetch_assoc()){
-                exit("non pui creare un'altro articolo con questo nome");
+                echo "non pui creare un'altro articolo con questo nome" ;
+                ?><meta http-equiv="refresh" content="5; url=creaArticolo"><?php
             }
             $dir = "articoli/".str_replace(" ", "", $_POST["titolo"]);
             mkdir($dir, 0755);
