b61e2fc7c0333ebd8b9c9646641af7b335ec4afb
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 08:31:52 +0100

    aggiornamento 2024 11 14 08:31

--- signIn.php
+++ signIn.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./css/login.css">
+        <?php require_once "metadati.php";?>
         <title>Sign In</title>
     </head>
     <?php
