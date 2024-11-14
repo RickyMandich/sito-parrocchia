efe06617d54358425983faf271f2210940d7a83d
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 16:10:58 +0100

    aggiornamento 2024 11 14 16:10

--- header.php
+++ header.php
@@ -10,19 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 function scanDirectory($directory){
-    // Verifica se la directory esiste
-    if (!file_exists($directory)) {
-        die("Errore: La directory $directory non esiste");
-    }
-    // Verifica i permessi
-    if (!is_readable($directory)) {
-        die("Errore: La directory $directory non è leggibile");
-    }
-    // Prova ad aprire la directory con gestione errori
     $dir = @opendir($directory);
-    if ($dir === false) {
-        die("Errore nell'apertura della directory: " . error_get_last()['message']);
-    }
     while (($file = readdir($dir)) !== false) {
         if ($file != '.' && $file != '..') {
             if(is_dir("$directory/$file")){
