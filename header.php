47431595a61dd14e6f3d85310aeeed92271e9665
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 12:02:26 +0100

    aggiornamento 2024 11 14 12:02

--- header.php
+++ header.php
@@ -25,9 +25,9 @@ function scanDirectory($directory){
     }
     while (($file = readdir($dir)) !== false) {
         if ($file != '.' && $file != '..') {
-            if(is_dir($file)){
+            if(is_dir("$directory/$file")){
                 echo "Cartella:".$file;
-                scanDirectory($directory.$file);
+                scanDirectory("$directory/$file");
             }else{
                 echo "File:$file<br>";
             }
