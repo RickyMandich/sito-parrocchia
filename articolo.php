571217741da27816b09d5f9e6b9c85688bad2a79
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 11:39:26 +0100

    aggiornamento 2024 11 14 11:39

--- articolo.php
+++ articolo.php
@@ -21,7 +21,7 @@
                         ?>
                     </div>
                     <div class="foto">
-                    <?php $directory = './cartella';
+                    <?php $directory = './articoli';
                     // Verifica se la directory esiste
                     if (!file_exists($directory)) {
                         die("Errore: La directory $directory non esiste");
