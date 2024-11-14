7be09f4546cfb131fb4389daa265bd0468c4a906
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 11:35:48 +0100

    aggiornamento 2024 11 14 11:35

--- articolo.php
+++ articolo.php
@@ -21,7 +21,7 @@
                         ?>
                     </div>
                     <div class="foto">
-                    <?php $dir = opendir('./articoli/'.$_GET["id"]);
+                    <?php $dir = opendir('./articoli');
                     var_dump($dir);
                     while (($file = readdir($dir)) !== false) {
                         if ($file != '.' && $file != '..') { // ignorare directory e parent directory
