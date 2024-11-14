bff8cd277c0ae60164cb204789696bec4ad88698
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 20:18:36 +0100

    aggiornamento 2024 11 14 20:18

--- creaArticolo.php
+++ creaArticolo.php
@@ -36,7 +36,7 @@ if(!isset($_SESSION["user"])){
                         <input type="text" name="titolo" placeholder="titolo" id="titolo">
                     </div>
                     <div class="form-group">
-                        <input type="textarea" name="contenuto" placeholder="contenuto" id="contenuto">
+                        <textarea name="contenuto" id="contenuto"></textarea>
                     </div>
                     <div class="form-group">
                         <input type="file" name="immagini[]" placeholder="file" id="immagini" multiple>
