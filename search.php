6b5222cb2626a1e086e017f714eef5b465b7dbd3
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 15:27:05 +0100

    aggiornamento 2024 11 14 15:27

--- search.php
+++ search.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="it">
     <head>
-        <?php if(!isset($_GET["titolo"]))?><meta http-equiv="refresh" content="3; url=./home">
+        <?php if(!isset($_GET["titolo"])){?>
+        <meta http-equiv="refresh" content="3; url=./home">
+        <?php } ?>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Hai certcato <?php $_GET["titolo"];?></title>
