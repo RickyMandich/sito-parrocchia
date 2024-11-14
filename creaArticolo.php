644af20873829a80b4bd313937b630881b7f1bb3
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 20:03:26 +0100

    aggiornamento 2024 11 14 20:03

--- creaArticolo.php
+++ creaArticolo.php
@@ -8,6 +8,7 @@ if(!isset($_SESSION["user"])){
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/login.css">
         <title>crea articolo</title>
     </head>
     <body>
