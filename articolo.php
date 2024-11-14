c95dbb11ba619fbd141d9601874cbb38d846a4bf
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 12:08:38 +0100

    aggiornamento 2024 11 14 12:08

--- articolo.php
+++ articolo.php
@@ -21,7 +21,7 @@
                         ?>
                     </div>
                     <div class="foto">
-                    <?php scandir("./articoli")?>
+                    <?php scanDirectory("./articoli")?>
                     </div>
                 <?php else:?>
                     <h1 class="maiuscolo">
