f242bf8447d60a9ce8d3c5a969a065daaea5146d
RickyMandich <ricky.mandich@gmail.com>
2024-11-14 19:47:14 +0100

    aggiornamento 2024 11 14 19:47

--- creaArticolo.php
+++ creaArticolo.php
@@ -18,7 +18,7 @@ if(!isset($_SESSION["user"])){
             $dir = "articoli/".str_replace(" ", "", $_POST["titolo"]);
             mkdir($dir, 0755);
             foreach($_FILES["immagini"]["tmp_name"] as $index => $tmp_name) {
-                $file_name = $_FILES["immagini"]["name"][$index];
+                $file_name = str_replace(" ", "", $_FILES["immagini"]["name"][$index]);
                 $destination = $articolo_dir . "/" . $file_name;
                 move_uploaded_file($tmp_name, $destination);
             }
