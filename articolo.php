e4978cfca3ca20dddf308d44359cfe782695295d
RickyMandich <ricky.mandich@gmail.com>
2024-11-16 19:58:36 +0100

    aggiornamento 2024 11 16 19:58

--- articolo.php
+++ articolo.php
@@ -9,32 +9,32 @@
         <?php
             require_once "intestazione.php";
             require_once "navigatore.php";
-            if(isset($_GET["id"])):
-                $articolo = $conn->query("select * from articoli where id = ".$_GET["id"])->fetch_assoc();
-                if($articolo):?>
-                    <h1 class="maiuscolo">
-                        <?php echo $articolo["titolo"];?>
-                    </h1>
-                    <div class="contenuto">
-                        <?php
-                            echo $articolo["contenuto"];
-                        ?>
-                        <div class="foto">
-                            <?php scanDirectory("./articoli/".str_replace(" ", "", $articolo["titolo"]));?>
+            if(isset($_SESSION["user"])):
+                if(isset($_GET["id"])):
+                    $articolo = $conn->query("select * from articoli where id = ".$_GET["id"])->fetch_assoc();
+                    if($articolo):?>
+                        <h1 class="maiuscolo">
+                            <?php echo $articolo["titolo"];?>
+                        </h1>
+                        <div class="contenuto">
+                            <?php
+                                echo $articolo["contenuto"];
+                            ?>
+                            <div class="foto">
+                                <?php scanDirectory("./articoli/".str_replace(" ", "", $articolo["titolo"]));?>
+                            </div>
                         </div>
-                    </div>
+                    <?php else:?>
+                        <h1 class="maiuscolo">
+                            articolo non trovato
+                        </h1>
+                    <?php endif;?>
                 <?php else:?>
                     <h1 class="maiuscolo">
                         articolo non trovato
                     </h1>
-                <?php
-                    endif;
-            else:?>
-                <h1 class="maiuscolo">
-                    articolo non trovato
-                </h1>
-            <?php
-                endif;
+                <?php endif;
+            endif;
             require_once "footer.php";
         ?>
     </body>
