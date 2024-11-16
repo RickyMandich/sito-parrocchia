46b80e874caafd43dffc8879522b5286563eaf0c
RickyMandich <ricky.mandich@gmail.com>
2024-11-16 22:00:38 +0100

    aggiornamento 2024 11 16 22:00

--- header.php
+++ header.php
@@ -18,7 +18,7 @@ function scanDirectory($directory){
                     scanDirectory("$directory/$file");
                 }else{
                     ?>
-                        <img src="<?php if(isset($_GET["edit"])) echo "../"?> <?php echo "../$directory/$file"?>" alt="<?php echo "../$directory/$file"?>">
+                        <img src="<?php if(isset($_GET["edit"])) echo "test"?> <?php echo "../$directory/$file"?>" alt="<?php echo "../$directory/$file"?>">
                     <?php
                 }
             }
