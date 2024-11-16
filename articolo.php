0f553846e7eb0cd498bbf9497a361c444e85cf43
RickyMandich <ricky.mandich@gmail.com>
2024-11-16 21:52:10 +0100

    aggiornamento 2024 11 16 21:52

--- articolo.php
+++ articolo.php
@@ -16,10 +16,10 @@
                         <?php echo $articolo["titolo"];?>
                         <?php if($_SESSION["user"]):?>
                             <form action="./edit">
-                                <img src="/img/edit.jpg" alt="modifica l'articolo">
+                                <input type="image" src="/img/edit.jpg" alt="modifica l'articolo">
                             </form>
                             <form action="/delete">
-                                <img src="/img/delete.jpg" alt="elimina l'articolo">
+                                <input type="image" src="/img/delete.jpg" alt="elimina l'articolo">
                             </form>
                         <?php endif;?>
                     </h1>
