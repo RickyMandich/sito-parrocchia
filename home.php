9907f1a570e23458dcae199db3e07f6a22696297
MandichRiccardo <ricky.mandich@gmail.com>
2024-12-31 13:29:57 +0100

    aggiornamento 2024 12 31 13:29

--- home.php
+++ home.php
@@ -9,7 +9,7 @@
         <?php require_once "intestazione.php";?>
         <?php require_once "navigatore.php";?>
         <main>
-            <?php $resultSet = $conn->query("select * from articoli order by pinnato, id desc");
+            <?php $resultSet = $conn->query("select * from articoli order by pinnato desc, id desc");
             while($articolo=$resultSet->fetch_assoc()){
                 ?>
                 <span class="anteprimaArticolo">
