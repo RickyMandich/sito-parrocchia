5c5712f2a35072d356164e2247156f3738f49bc4
MandichRiccardo <ricky.mandich@gmail.com>
2024-12-31 13:10:35 +0100

    aggiornamento 2024 12 31 13:10

--- search.php
+++ search.php
@@ -50,10 +50,12 @@
                         </form>
                     <?php } ?>
                 </h2>
-                <?php echo substr($articolo["contenuto"], 0, 600);?>
+                <?php echo substr($articolo["contenuto"], 0, 360);?>
             </span>
             <?php
         };?>
         <?php require_once "footer.php";?>
     </body>
 </html>
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi
