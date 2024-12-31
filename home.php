f32fb8d7bad464b0bc36e0b9d41dd34ff625f43d
MandichRiccardo <ricky.mandich@gmail.com>
2024-12-31 13:14:33 +0100

    aggiornamento 2024 12 31 13:14

--- home.php
+++ home.php
@@ -27,7 +27,7 @@
                         &#128204;
                     <?php } ?>
                     </h2>
-                    <?php echo substr($articolo["contenuto"], 0, [$length = 60]);?>
+                    <?php echo substr($articolo["contenuto"], 0, 60);?>
                 </span>
                 <?php
             };?>
