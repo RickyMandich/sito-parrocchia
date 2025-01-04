0a3c036ccd6d7bd1ce370577794eeaf15f0c13b4
RickyMandich <ricky.mandich@gmail.com>
2025-01-04 14:48:54 +0100

    aggiornamento 2025 01 04 14:48

--- navigatore.php
+++ navigatore.php
@@ -1,11 +1,11 @@
 <?php
     require_once "header.php";
     function hasSubElement($id=0){
-        return $GLOBALS["conn"]->query("select *  from menu where padre = $id")->fetch_assoc();
+        return $GLOBALS["conn"]->query("select * from navigatore where padre = $id")->fetch_assoc();
     }
     function generaMenu($id=0){
         $menu = [];
-        $result = $GLOBALS["conn"]->query("select nome, link from menu where padre = $id order by id");
+        $result = $GLOBALS["conn"]->query("select nome, link from navigatore where padre = $id order by id");
         while($row = $result->fetch_assoc()){
             array_push($menu, $row);
         }
