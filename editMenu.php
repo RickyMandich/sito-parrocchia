d7523183df70a60452f0fb2beac0b1ee25acff83
RickyMandich <ricky.mandich@gmail.com>
2025-01-07 15:58:24 +0100

    aggiornamento 2025 01 07 15:58

--- editMenu.php
+++ editMenu.php
@@ -11,8 +11,59 @@
         <?php
             require_once "intestazione.php";
             require_once "navigatore.php";
+            function getEredi($id){
+                $result = $GLOBALS["conn"]->query("select * from navigatore where padre = $id");
+                $figli = [];
+                while($row = $result->fetch_assoc()){
+                    array_push($figli, $row);
+                    $nipoti = getEredi($row['id']);
+                    foreach($nipoti as $nipote){
+                        array_push($figli, $nipote);
+                    }
+                }
+                return $figli;
+            }
             if(isset($_GET['id'])){
-                
+                $id = $_GET['id'];
+                $query = "SELECT * FROM navigatore WHERE id = $id";
+                $result = $conn->query($query);
+                $row = $result->fetch_assoc();
+
+                $nome = $row['nome'];
+                $link = $row['link'];
+                $padre = $row['padre'];
+
+                $eredi = getEredi($id);
+
+                $query = "SELECT * FROM navigatore WHERE id != $id";
+                $result = $conn->query($query);
+                while($row = $result->fetch_assoc()){
+                    if(!in_array($row, $eredi)){
+                        array_push($nodi, $row);
+                    }
+                }
+                ?>
+
+                <form method="get" action="/editMenu/<?php echo $id; ?>">
+                    <label for="nome">Nome:</label>
+                    <input type="text" id="nome" name="nome" value="<?php echo htmlspecialchars($nome); ?>" required>
+                    <br>
+                    <label for="link">Link:</label>
+                    <input type="text" id="link" name="link" value="<?php echo htmlspecialchars($link); ?>" required>
+                    <br>
+                    <label for="padre">Padre:</label>
+                    <select id="padre" name="padre">
+                        <option value="0">Nessuno</option>
+                        <?php foreach ($nodi as $row): ?>
+                            <option value="<?php echo $row['id']; ?>" <?php if ($row['id'] == $padre) echo 'selected'; ?>>
+                                <?php echo htmlspecialchars($row['nome']); ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                    <br>
+                    <input type="submit" value="Salva">
+                </form>
+                <?php
             }else{
                 generaMenu(edit:true);
             }
