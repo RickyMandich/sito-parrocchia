4d4ec8cc4ad46d4f8a38917e4e392d0712383aa8
RickyMandich <ricky.mandich@gmail.com>
2025-01-07 11:07:00 +0100

    aggiornamento 2025 01 07 11:07

--- editMenu.php
+++ editMenu.php
@@ -14,7 +14,7 @@
             if(isset($_GET['id'])){
                 
             }else{
-                generaMenu();
+            generaMenu(edit:true);
             }
             require_once "footer.php";
         ?>
--- navigatore.php
+++ navigatore.php
@@ -3,7 +3,7 @@
     function hasSubElement($id=0){
         return $GLOBALS["conn"]->query("select * from navigatore where padre = $id")->fetch_assoc();
     }
-    function generaMenu($id=0){
+    function generaMenu($id=0, $edit = false){
         $menu = [];
         $result = $GLOBALS["conn"]->query("select id, nome, link from navigatore where padre = $id order by id");
         while($row = $result->fetch_assoc()){
@@ -13,7 +13,7 @@
         foreach($menu as $voce){
             ?>
                 <li>
-                    <a href='<?php echo $voce['link']?>'>
+                    <a href='<?php echo $edit?$voce['link']:"/editMenu/".$voce['id']?>'>
                         <?php echo $voce['nome']?>
                     </a>
                     <?php if(hasSubElement($voce['id'])) generaMenu($voce['id']); ?>
