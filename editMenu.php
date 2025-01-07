04c20af945c86f2f583da0ab84f379045e4f1246
RickyMandich <ricky.mandich@gmail.com>
2025-01-07 16:18:10 +0100

    aggiornamento 2025 01 07 16:18

--- editMenu.php
+++ editMenu.php
@@ -76,6 +76,28 @@
             }else{
                 generaMenu(edit:true);
             }
+            ?>
+            <form action="newMenu">
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome" required>
+                <label for="link">Link:</label>
+                <input type="text" id="link" name="link" required>
+                <label for="padre">Padre:</label>
+                <select id="padre" name="padre">
+                    <option value="0">Nessuno</option>
+                    <?php
+                        $result = $conn->query("select * from navigatore");
+                        while($row = $result->fetch_assoc()){
+                            ?>
+                                <option value="<?php echo $row['id']; ?>">
+                                    <?php echo $row['nome']; ?>
+                                </option>
+                            <?php
+                        }
+                    ?>
+                <input type="submit" value="crea">
+            </form>
+            <?php
             require_once "footer.php";
         ?>
     </body>
