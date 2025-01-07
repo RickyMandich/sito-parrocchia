98b620d34b429dff6d979f7596557862a6b5de26
RickyMandich <ricky.mandich@gmail.com>
2025-01-07 10:58:07 +0100

    aggiornamento 2025 01 07 10:58

---
+++ editMenu.php
@@ -0,0 +1,18 @@
+<?php require_once "header.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>modifica navigatore</title>
+    </head>
+    <body>
+        <?php
+            if(isset($_GET['id'])){
+                
+            }else{
+                generaMenu();
+            }
+        ?>
+    </body>
+</html>
