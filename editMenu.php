a9acc0435f781b2fe254c9239460d21715caae85
RickyMandich <ricky.mandich@gmail.com>
2025-01-07 11:01:24 +0100

    aggiornamento 2025 01 07 11:01

--- editMenu.php
+++ editMenu.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php require_once "metadati.php"; ?>
         <title>modifica navigatore</title>
     </head>
     <body>
